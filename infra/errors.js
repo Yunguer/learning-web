1e9f5e67dab24fb0b7407bc1853e566c91662c09
Yunguer Dvorak Mendes <yunguerd@gmail.com>
2026-03-19 20:14:57 -0300

    feat: create and use `NotFoundError` in `controller.js`

--- infra/errors.js
+++ infra/errors.js
@@ -76,3 +76,23 @@ export class ValidationError extends Error {
     };
   }
 }
+
+export class NotFoundError extends Error {
+  constructor({ cause, message, action }) {
+    super(message || "Não foi possivel encontrar o recurso solicitado.", {
+      cause,
+    });
+    this.name = "NotFoundError";
+    this.action = action || "Verifique os parâmetros e tente novamente.";
+    this.statusCode = 404;
+  }
+
+  toJSON() {
+    return {
+      name: this.name,
+      message: this.message,
+      action: this.action,
+      status_code: this.statusCode,
+    };
+  }
+}
